e42f04160131b5d530f1e781fd3ed13d74888a76
Davis Rapp <davisrapp@yahoo.com>
2025-09-30 18:25:31 +0000

    Complete ML pipeline with FastAPI deployment

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,52 @@
 import pytest
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 # TODO: add necessary import
-
+from ml.model import train_model, compute_model_metrics, inference
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model():
     """
-    # add description for the first test
+    Test that train_model returns a RandomForestClassifier
     """
-    # Your code here
-    pass
+    # Create Sample Data
+    X_train = np.random.rand(100, 10)
+    y_train = np.random.randint(0, 2, 100)
+    # train model 
+    model = train_model(X_train, y_train)
+    #Check that it returns RCF
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_calculate_model_metrics():
     """
-    # add description for the second test
+    Test that compute_model_metrics returns correct values
     """
-    # Your code here
-    pass
+    # create sample true and predicted values
+    y_true = np.array([1, 0, 1, 1, 0, 1, 0, 0])
+    y_pred = np.array([1, 0, 1, 1, 0, 1, 0, 0])
+    # calculate metrics
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert precision == 1.0
+    assert recall == 1.0
+    assert fbeta == 1.0
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference():
     """
-    # add description for the third test
+    Test that inference returns predictions of correct shape
     """
-    # Your code here
-    pass
+    # Create and trains simple model
+    X_train = np.random.rand(100, 10)
+    y_train = np.random.randint(0, 2, 100)
+    model = train_model(X_train, y_train)
+    # Create test data
+    X_test = np.random.rand(20, 10)
+    # Get predictions
+    preds = inference (model, X_test)
+    # Check predictions
+    assert preds.shape[0] == X_test.shape[0]
+    assert all(p in [0, 1] for p in preds)
